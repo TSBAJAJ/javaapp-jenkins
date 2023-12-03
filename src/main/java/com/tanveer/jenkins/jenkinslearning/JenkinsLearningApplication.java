1c6fd5d6c8bbc823cf9d32dddc4f02cfb8a8f0e3
Tanveer Singh <tsbajaj28@gmail.com>
2023-12-03 23:51:35 +0530

    some changes

--- src/main/java/com/tanveer/jenkins/jenkinslearning/JenkinsLearningApplication.java
+++ src/main/java/com/tanveer/jenkins/jenkinslearning/JenkinsLearningApplication.java
@@ -6,8 +6,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JenkinsLearningApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(JenkinsLearningApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsLearningApplication.class, args);
+        System.out.println("application started");
+
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.println("thread completed");
+        System.exit(0);
+    }
 
 }
