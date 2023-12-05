8f0817ec8ecf7ae2b5416fd47d23c9dce5eaacb9
Tanveer Singh <tsbajaj28@gmail.com>
2023-12-05 22:41:18 +0530

    added api

--- src/main/java/com/tanveer/jenkins/jenkinslearning/JenkinsLearningApplication.java
+++ src/main/java/com/tanveer/jenkins/jenkinslearning/JenkinsLearningApplication.java
@@ -2,22 +2,15 @@ package com.tanveer.jenkins.jenkinslearning;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class JenkinsLearningApplication {
+public class JenkinsLearningApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(JenkinsLearningApplication.class, args);
         System.out.println("application started");
-
-//        try {
-//            Thread.sleep(5000);
-//        } catch (InterruptedException e) {
-//            throw new RuntimeException(e);
-//        }
-
         System.out.println("thread completed");
-//        System.exit(0);
     }
 
 }
--- src/main/java/com/tanveer/jenkins/jenkinslearning/controller/TestController.java
+++ src/main/java/com/tanveer/jenkins/jenkinslearning/controller/TestController.java
@@ -1,12 +1,14 @@
 package com.tanveer.jenkins.jenkinslearning.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/controller")
 public class TestController {
 
-    @RequestMapping("/test")
+    @GetMapping("/test")
     public String test() {
         return "success";
     }
