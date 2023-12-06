3ae9dff53fb3775ffea78cdd8b5bde81510a8387
Tanveer Singh <tsbajaj28@gmail.com>
2023-12-06 23:20:09 +0530

    testfile

--- src/test/java/com/tanveer/jenkins/jenkinslearning/JenkinsLearningApplicationTests.java
+++ src/test/java/com/tanveer/jenkins/jenkinslearning/JenkinsLearningApplicationTests.java
@@ -3,11 +3,14 @@ package com.tanveer.jenkins.jenkinslearning;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 @SpringBootTest
 class JenkinsLearningApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+        assertTrue(true);
+    }
 
 }
